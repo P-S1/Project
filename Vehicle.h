21e56a1720b7fe808d540eade43575027b3d2a9d
khaivo0203 <161456676+khaivo0203@users.noreply.github.com>
2024-05-14 11:57:33 +0930

    Create Vehicle.h

---
+++ Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+    private:
+        string make;
+        string model;
+        float price;
+    public:
+        Vehicle();
+        Vehicle(string make, string model, float price);
+        virtual void display_information();
+        float get_price();
+        void set_price(float price);
+};
+#endif
