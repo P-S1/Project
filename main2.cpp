13200b8b985b9fb6c46fb82dea7e16a346d0b7df
P-S1 <pranav.singh@student.adelaide.edu.au>
2024-05-18 20:33:24 +0930

    updated trucks and motorbike

--- main2.cpp
+++ main2.cpp
@@ -2,6 +2,25 @@
 #include "Truck.h" 
 
 
+
+int main() {
+    std::vector<Truck> truckList = {
+        Truck("Volvo", "FH16", 120000, "Automatic", "Diesel", 25.0),
+        Truck("Mercedes-Benz", "Actros", 110000, "Automatic", "Diesel", 24.0),
+        Truck("Scania", "R500", 115000, "Automatic", "Diesel", 22.0),
+        Truck("MAN", "TGX", 105000, "Automatic", "Diesel", 24.5),
+        Truck("DAF", "XF", 95000, "Automatic", "Diesel", 23.0),
+        Truck("Iveco", "Stralis", 98000, "Automatic", "Diesel", 21.5),
+        Truck("Freightliner", "Cascadia", 125000, "Automatic", "Diesel", 26.0),
+        Truck("Kenworth", "W900", 130000, "Manual", "Diesel", 24.0),
+        Truck("Peterbilt", "579", 119000, "Automatic", "Diesel", 25.5),
+        Truck("Mack", "Anthem", 108000, "Automatic", "Diesel", 23.5)
+    };
+
+
+
+
+
 int main() {
     std::vector<Truck> truckList = {
         Truck("Ford", "F-150", 15000, "Automatic", "Gasoline", 5, "Pickup Truck"),
--- main3.cpp
+++ main3.cpp
@@ -4,6 +4,32 @@
 
 int main() {
     std::vector<Motorbike> motorbikeList = {
+        Motorbike("Yamaha", "MT-07", 7500, "Manual", "Gasoline", 689),
+        Motorbike("Honda", "CBR500R", 6500, "Manual", "Gasoline", 471),
+        Motorbike("Kawasaki", "Z650", 7200, "Manual", "Gasoline", 649),
+        Motorbike("Ducati", "Monster 797", 9000, "Manual", "Gasoline", 803),
+        Motorbike("BMW", "G 310 R", 4750, "Manual", "Gasoline", 313),
+        Motorbike("Suzuki", "SV650", 7000, "Manual", "Gasoline", 645),
+        Motorbike("KTM", "390 Duke", 5500, "Manual", "Gasoline", 373),
+        Motorbike("Yamaha", "YZF-R3", 5300, "Manual", "Gasoline", 321),
+        Motorbike("Honda", "Rebel 500", 6200, "Manual", "Gasoline", 471),
+        Motorbike("Harley-Davidson", "Iron 883", 9000, "Manual", "Gasoline", 883),
+        Motorbike("Triumph", "Street Triple", 9500, "Manual", "Gasoline", 765),
+        Motorbike("Ducati", "Scrambler Icon", 8500, "Manual", "Gasoline", 803),
+        Motorbike("Kawasaki", "Ninja 400", 5000, "Manual", "Gasoline", 399),
+        Motorbike("Suzuki", "GSX250R", 4500, "Manual", "Gasoline", 248),
+        Motorbike("BMW", "R NineT", 15000, "Manual", "Gasoline", 1170),
+        Motorbike("Yamaha", "Tracer 900", 11000, "Manual", "Gasoline", 847),
+        Motorbike("Honda", "Africa Twin", 14000, "Manual", "Gasoline", 998),
+        Motorbike("KTM", "1290 Super Duke R", 18000, "Manual", "Gasoline", 1301),
+        Motorbike("Kawasaki", "Versys 650", 8300, "Manual", "Gasoline", 649),
+        Motorbike("Triumph", "Bonneville T120", 11500, "Manual", "Gasoline", 1200)
+    };
+
+
+
+int main() {
+    std::vector<Motorbike> motorbikeList = {
         Motorbike("Yamaha", "YFZ", 5000, "Automatic", "Electric", 2, "Scooter"),
         Motorbike("Yamaha", "YZF-R1", 1100, "Manual", "Gasoline", 2, "Motorcycle"),
         Motorbike("Yamaha", "MT-09", 200, "Manual", "Gasoline", 2, "Motorcycle"),
